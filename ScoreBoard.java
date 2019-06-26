f7dd5219edeff7557b9783c04583d773f949b027
Kiran Karia <44964727+KiranKaria@users.noreply.github.com>
2019-06-26 09:30:58 +0200

    .

--- ScoreBoard.java
+++ ScoreBoard.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Scoreboard extends Actor
+public class Scoreboard.java extends Actor
 {
     
     private int aantalDiamanten = 0;
